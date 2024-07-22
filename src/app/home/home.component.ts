af9d6b906af98afe490ac1d7b9c946fd5e4b0193
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-07-22 10:00:14 +0100

    Task 11: start crud read operation & defining the api of our service layer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,10 +23,6 @@ import {
   outputFromObservable,
 } from '@angular/core/rxjs-interop';
 
-type Counter = {
-  value: number;
-};
-
 @Component({
   selector: 'home',
   standalone: true,
@@ -35,48 +31,7 @@ type Counter = {
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  public counter = signal(0);
-  public effectRef: EffectRef | null = null;
+  public courses = signal<Course[]>([]);
 
-  tenCounter = computed(() => {
-    const val = this.counter();
-    return val * 10;
-  });
-
-  hundredCounter = computed(() => {
-    const val = this.counter();
-    return val * 100;
-  });
-
-  constructor() {
-    this.effectRef = effect((onCleanup) => {
-      /**
-       * You need to create the dependency of effect before anything else
-       * So att the beginning of the effect, create all your dependencies and then use them in the body of effect
-       */
-      const counter = this.counter();
-      const timeout = setTimeout(() => {
-        console.log(`counter value 1: ${counter}`);
-      }, 1000);
-
-      onCleanup(() => {
-        console.log('Calling clean up function');
-        clearTimeout(timeout);
-      });
-    });
-  }
-
-  public increment() {
-    this.counter.update((val) => val + 1);
-  }
-  public decrement() {
-    const currentCounter = this.counter();
-    if (currentCounter > 0) {
-      this.counter.update((val) => val - 1);
-    }
-  }
-
-  public cleanup() {
-    this.effectRef?.destroy();
-  }
+  private coursesService = inject(CoursesService);
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,15 +1,17 @@
-import {Injectable} from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import {environment} from "../../environments/environment";
-import {firstValueFrom} from "rxjs";
-import {Course} from "../models/course.model";
-import {GetCoursesResponse} from "../models/get-courses.response";
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Course } from '../models/course.model';
+import { GetCoursesResponse } from '../models/get-courses.response';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CoursesService {
-
-
+  //loadAllCourses():Observable<Course[]> {} old way using rxJs
+  
+  async loadAllCourses(): Promise<Course[]> {
+    return [];
+  }
 }
