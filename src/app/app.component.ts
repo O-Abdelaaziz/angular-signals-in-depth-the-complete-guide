07bfe762c6fcb503623e35fcbd6ebfdaedad90db
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-07-23 10:57:42 +0100

    Task 34: implement login feature & adapting the ui to the user authentication status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,37 @@
-import {Component, inject} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
-import {MatListItem, MatNavList} from "@angular/material/list";
-import {MatIcon} from "@angular/material/icon";
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatIconButton} from "@angular/material/button";
-import {LoadingIndicatorComponent} from "./loading/loading.component";
-import {MessagesComponent} from "./messages/messages.component";
-
+import { AuthService } from './services/auth.service';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import { LoadingIndicatorComponent } from './loading/loading.component';
+import { MessagesComponent } from './messages/messages.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, MatIcon, RouterLink, MatToolbar,
-    MatIconButton, LoadingIndicatorComponent, MessagesComponent],
+    RouterOutlet,
+    CommonModule,
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    MatIcon,
+    RouterLink,
+    MatToolbar,
+    MatIconButton,
+    LoadingIndicatorComponent,
+    MessagesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  public authService = inject(AuthService);
 
-
+  public isLoggedIn = this.authService.isLoggedIn;
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,30 @@
-import {computed, effect, inject, Injectable, signal} from "@angular/core";
-import {User} from "../models/user.model";
-import {environment} from "../../environments/environment";
-import {Router} from "@angular/router";
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { User } from '../models/user.model';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 const USER_STORAGE_KEY = 'user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  #userSignal = signal<User | null>(null);
+  public user = this.#userSignal.asReadonly();
 
+  public isLoggedIn = computed(() => !!this.user());
+  private httpClient = inject(HttpClient);
 
+  public async login(email: string, password: string): Promise<User> {
+    const login$ = this.httpClient.post<User>(`${environment.apiRoot}/login`, {
+      email,
+      password,
+    });
+
+    const user = await firstValueFrom(login$);
+    this.#userSignal.set(user);
+    return user;
+  }
 }
