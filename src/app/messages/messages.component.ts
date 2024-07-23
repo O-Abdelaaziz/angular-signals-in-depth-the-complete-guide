d49d33c078f9f050913619dc16efe0307a0a8588
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-07-23 10:18:05 +0100

    Task 31: implement message component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,17 +1,19 @@
-import {Component, inject} from "@angular/core";
-import {MessagesService} from "./messages.service";
-import {NgClass} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { MessagesService } from './messages.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss'],
-  imports: [
-    NgClass
-  ],
-  standalone: true
+  imports: [NgClass],
+  standalone: true,
 })
 export class MessagesComponent {
+  private messageService = inject(MessagesService);
+  public message = this.messageService.message;
 
-
+  public onClose() {
+    this.messageService.hideMessage();
+  }
 }
