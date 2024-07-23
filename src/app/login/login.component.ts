23fb7cc6ad51b46e900de4207bc3050a1c41fa6b
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-07-23 10:43:22 +0100

    Task 33: implementing the login form in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,40 @@
-import {Component, inject} from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
-import {AuthService} from "../services/auth.service";
-import {MessagesService} from "../messages/messages.service";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MessagesService } from '../messages/messages.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [
-    RouterLink,
-    ReactiveFormsModule
-  ],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
+  private messagesService = inject(MessagesService);
 
+  public loginFormGroup = this.formBuilder.group({
+    email: [''],
+    password: [''],
+  });
 
+  public onLogin() {
+    try {
+      const { email, password } = this.loginFormGroup.value;
+      if (!email || !password) {
+        this.messagesService.showMessage(
+          'email or password is mandatory!',
+          'warning'
+        );
+      }
+      
+    } catch (error) {
+      this.messagesService.showMessage(
+        'Login failed, please try again: ' + error,
+        'error'
+      );
+    }
+  }
 }
