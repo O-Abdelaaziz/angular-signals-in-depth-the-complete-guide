acfe18d454f025602c247d2f9cf3c1384528ad16
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-07-23 14:22:41 +0100

    Task 44: implementing the lesson service (load all lessons)

--- src/app/services/lessons.service.ts
+++ src/app/services/lessons.service.ts
@@ -1,16 +1,39 @@
-import {inject, Injectable} from "@angular/core";
-import {Lesson} from "../models/lesson.model";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import {firstValueFrom} from "rxjs";
-import {GetLessonsResponse} from "../models/get-lessons.response";
-import {environment} from "../../environments/environment";
-
+import { inject, Injectable } from '@angular/core';
+import { Lesson } from '../models/lesson.model';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { GetLessonsResponse } from '../models/get-lessons.response';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LessonsService {
-
   env = environment;
+  http = inject(HttpClient);
 
+  // public async loadAllLessons(): Promise<Lesson[]> {
+  //   const response = await firstValueFrom(this.http.get<GetLessonsResponse>(`${this.env.apiRoot}/lessons`));
+  //   return response.lessons;
+  // }
+
+  public async loadAllLessons(config: {
+    courseId?: string;
+    query?: string;
+  }): Promise<Lesson[]> {
+    const { courseId, query } = config;
+    let params = new HttpParams();
+    if (courseId) {
+      params = params.set('courseId', courseId);
+    }
+    if (query) {
+      params = params.set('query', query);
+    }
+    const lesson$ = this.http.get<GetLessonsResponse>(
+      `${this.env.apiRoot}/search-lessons`,
+      { params }
+    );
+    const response = await firstValueFrom(lesson$);
+    return response.lessons;
+  }
 }
